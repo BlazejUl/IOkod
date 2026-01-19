793cf676ae847560a4f08eb35e8fc6fd50c09c3f
blazej <blazej.uliasz2004@gmail.com>
2026-01-19 19:01:47 +0100

    dodanie @-tów

--- src/src/test/java/kontroler/TestZmianaDanychOsobowych.java
+++ src/src/test/java/kontroler/TestZmianaDanychOsobowych.java
@@ -3,6 +3,9 @@ package kontroler;
 
 import model.Model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 
@@ -12,12 +15,18 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
+@DisplayName("test klasy ZmianaDanychOsobowych")
 public class TestZmianaDanychOsobowych {
 
 
 
     @Test
+    @DisplayName("Test inicjalizacji")
+    @Tag("kontroler")
+    @Tag("inicjalizacja")
+    @Order(1)
     public void TestInicjalizacij(){
+        //Jeśli
         ArrayList noweDane = new ArrayList<String>();
         noweDane.add("Adam");
         noweDane.add("Kowalski");
@@ -28,14 +37,20 @@ public class TestZmianaDanychOsobowych {
 
         Model mockModel = mock(Model.class);
         doNothing().when(mockModel).zmianaDanychOsobowychObywatela(111,noweDane);
+        //Gdy
         ZmianaDanychOsobowych zmianaDanychOsobowych = new ZmianaDanychOsobowych(mockModel,111,noweDane);
-
+        //Wtedy
         verify(mockModel, times(1)).zmianaDanychOsobowychObywatela(111,noweDane);
     }
 
 
     @Test
+    @DisplayName("Test funkcji wprowadzania danych")
+    @Tag("kontroler")
+    @Tag("funkcja")
+    @Order(2)
     public void TestWprowadzenieDanychOsobowych(){
+        //Jeśli
         ArrayList noweDane = new ArrayList<String>();
         noweDane.add("Adam");
         noweDane.add("Kowalski");
@@ -55,9 +70,12 @@ public class TestZmianaDanychOsobowych {
         Model mockModel = mock(Model.class);
         doNothing().when(mockModel).zmianaDanychOsobowychObywatela(111,noweDane);
         doNothing().when(mockModel).zmianaDanychOsobowychObywatela(111,Dane);
+
+        //Gdy
         ZmianaDanychOsobowych zmianaDanychOsobowych = new ZmianaDanychOsobowych(mockModel,111,noweDane);
         zmianaDanychOsobowych.wprowadzenieDanychOsobowych(111,Dane);
 
+        //Wtedy
         verify(mockModel, times(2)).zmianaDanychOsobowychObywatela(111,noweDane);
     }
 }
