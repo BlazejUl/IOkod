97f688ef394fc1809e23797a44531801c00803c2
blazej <blazej.uliasz2004@gmail.com>
2026-01-27 17:59:23 +0100

    finesse

---
+++ src/src/test/java/testyFinesse/SetUp.java
@@ -0,0 +1,19 @@
+package testyFinesse;
+
+
+import fit.Fixture;
+import model.DAO;
+import model.Model;
+import model.SpisObywateli;
+
+public class SetUp extends Fixture {
+    static DAO dao;
+    static Model model;
+    static SpisObywateli spis;
+
+    public SetUp(){
+        dao = new DAO();
+        spis = new SpisObywateli(dao);
+        model = new Model(spis,dao);
+    }
+}
